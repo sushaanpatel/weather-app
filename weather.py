cd33bab117ea00843a79e811eecd712be5ac338f
sushaanpatel <sushaanpatel@gmail.com>
2021-06-12 14:14:26 +0530

    adding basic cli app

--- weather.py
+++ weather.py
@@ -1,6 +1,31 @@
-#Created By Sushaan Patel
-def start():
-    wther_loca = str(input("Location : "))
-    print(wther_loca)
+#Created by Sushaan Patel
+import os
+import dotenv
+import requests, json
 
-start()
+dotenv.load_dotenv()
+api_key = os.environ.get('API_KEY')
+
+def main():
+    location = str(input("Enter City Name : ")).lower()
+    info = {'q' : location, "appid" : api_key, "units" : "metric"}
+    r = requests.get(f"http://api.openweathermap.org/data/2.5/weather?", params=info)
+    weather = r.json()
+    if weather["cod"] == '404':
+        print("\nCity not found")
+    else:
+        print("\n-----------")
+        print("   Basic   ")
+        print("-----------")
+        print("Temperature : " + str(round(weather["main"]["temp"])) + u"\N{DEGREE SIGN}C")
+        print("Feels Like : " + str(round(weather["main"]["feels_like"])) + u"\N{DEGREE SIGN}C")
+        print("Pressure : " + str(round(weather["main"]["pressure"])) +  "mBar")
+        print("Humidity : " + str(round(weather["main"]["humidity"])) + "%")
+        print("-------------")
+        print("Wind & Clouds")
+        print("-------------")
+        print("Clouds : " + weather["weather"][0]["description"])
+        print("Wind Speed : " + str(round(weather["wind"]["speed"] * 3.6, 2)) + " km/h")
+        print("Wind Direction : " + str(weather["wind"]["deg"]) + u"\N{DEGREE SIGN} from N")
+
+main()
